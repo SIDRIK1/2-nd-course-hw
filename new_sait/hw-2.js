bf8c932926361bcef23a01850924ea1a1acb9d03
ЫSIDRIK1 <sidorov56782@yandex.ru>
2025-10-26 20:03:23 +0400

    hw2

---
+++ new_sait/hw-2.js
@@ -0,0 +1,63 @@
+// task 1
+
+let a =10 
+alert(a)
+a = 20
+alert(a)
+// task 2
+
+let dateIphoneCreate = 2007
+alert(dateIphoneCreate)
+// task 3
+
+let creatorJS = 'Brendan Eich'
+alert(creatorJS)
+// task 4 
+
+let first = 10
+let second = 2
+
+alert(
+    "Сложение " + (first+second) + '\n' +
+    "Вычитание " + (first-second) + '\n' +
+    "Умножение " + (first*second) + '\n' +
+    "Деление " + (first/second) + '\n'
+    
+)
+// task 5 
+
+let result = 2**5
+alert(result)
+// task 6 
+// Вместо а буду использовать переменную с 
+
+let с = 9 
+let b = 2
+
+alert(c%b)
+// task 7 
+
+let num = 1
+num += 5
+num -= 3
+num *= 7
+num /= 3
+num += 1
+num -= 1
+alert(num)
+// task 8 
+
+age = Number(prompt('Сколько вам лет: '))
+alert(age)
+// task 9 
+
+let user = {
+    name: 'Ivan',
+    age: 12,
+    isAdmin: true
+}
+
+// task 10 
+
+let nameUser = prompt('Как вас зовут: ')
+alert(`Привет ${nameUser}`)
